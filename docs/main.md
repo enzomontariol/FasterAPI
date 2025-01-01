0a639c48ad9b89565fa8d4cfb2b0cc82df803119
paulbourcereau <paul.bourcereau@icloud.com>
2025-01-01 07:14:44 +0100

    Fix oubli Désolé pour le commit en retard, j'avais oublié certaines de mes dernières modifications Bonne année :)

--- docs/main.md
+++ docs/main.md
@@ -26,7 +26,32 @@ def add_numbers(a: int, b: int):
 ```
 Supported methods are ```GET```, ```POST```, ```PUT```, ```DELETE``` and ```PATCH```
 
-The only parameter for decorator is ```path```, which indicates the path to be used by the router. It is optional and will be infered from the function name if left blank.  
+There are two optional parameters for the decorator : 
+
+- ```path```, which indicates the path to be used by the router. It is optional and will be infered from the function name if left blank.  
+- ```middleware``` which is a list of middlewares to be used when responding to the request.  
+
+## Using middlewares
+
+Middlewares can be used to perform authentication, compute time to complete a request etc... 
+They are functions that are passed to the decorator, that will be ran when a request is made to the endpoint. 
+They take in two parameters : 
+
+-  ```request```, a FastAPI Request object that contains all informations about the request, such as headers. 
+- ```func``` the function that is decorated. 
+
+If the middleware returns a value or raises an exception, it is sent as the response and the process ends there. 
+
+Example for a simple authentication process : 
+```python
+def check_access_token(request, func):
+    if request.headers.get("Authorization", "") != "123":
+        raise Exception("Access denied")
+
+@app.get("/restricted-access", middleware=[check_access_token])
+def restricted_access(request: Request):
+    return {"message": "Successfull access to restricted content"}
+```
 
 ## Starting the server 
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,24 +1,29 @@
 from faster_api import FasterAPI
 from dataclasses import dataclass
 from fastapi.testclient import TestClient
+from fastapi import Request
 
 import unittest
 
 app = FasterAPI()
 
+
 @app.get("/")
 def root():
     return {"message": "Bienvenue sur l'API"}
+
 
 # Example with GET
 @app.get()
 def test():
     return {"test": "test"}
 
+
 @dataclass
 class Stock:
     name: str
     price: float
+
 
 # Example with POST
 @app.post("/test-stock")
@@ -27,6 +32,7 @@ def stock(stock: Stock):
     Endpoint pour tester une classe Stock.
     """
     return {"name": stock.name, "price": stock.price}
+
 
 # Function to add 2 numbers
 @app.get("/add")
@@ -37,37 +43,62 @@ def add_numbers(a: int, b: int):
     result = a + b
     return {"a": a, "b": b, "sum": result}
 
-client = TestClient(app)
+
+def check_access_token(request, func):
+    if request.headers.get("Authorization", "") != "123":
+        raise Exception("Access denied")
+
+
+@app.get("/restricted-access", middleware=[check_access_token])
+def restricted_access(request: Request):
+    return {"message": "Successfull access to restricted content"}
+
 
 # === Unit testings ===
 
+
 class TestApp(unittest.TestCase):
-    
+
+    def setUp(self):
+        self.client = TestClient(app)
+
     def test_root(self):
         """
         Test de la route racine.
         """
-        response = client.get("/")
-        assert response.status_code == 200
-        assert response.json() == {"message": "Bienvenue sur l'API"}
-
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "Bienvenue sur l'API"})
 
     def test_add_numbers(self):
         """
         Test de l'addition de deux nombres.
         """
-        response = client.get("/add?a=5&b=7")
-        assert response.status_code == 200
-        assert response.json() == {"a": 5, "b": 7, "sum": 12}
+        response = self.client.get("/add?a=5&b=7")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"a": 5, "b": 7, "sum": 12})
 
     def test_stock(self):
         """
         Test de la route test_stock.
         """
         payload = {"name": "Apple", "price": 150.0}
-        response = client.post("/test-stock", json=payload)
-        assert response.status_code == 200
-        assert response.json() == {"name": "Apple", "price": 150.0}
+        response = self.client.post("/test-stock", json=payload)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"name": "Apple", "price": 150.0})
+
+    def test_restricted_access(self):
+        """
+        Test de la route restricted-access.
+        """
+        response = self.client.get(
+            "/restricted-access", headers={"Authorization": "123"}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(), {"message": "Successfull access to restricted content"}
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
